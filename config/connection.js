3e6812ff3009a6a665f1de87869d211711daae27
GreenCord <steve@nevizen.com>
2018-02-04 11:35:07 -0500

    Updated connection for Heroku/JawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 var mysql = require('mysql');
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: process.env.MYSQLPW,
-	database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: process.env.MYSQLPW,
+		database: 'burgers_db'
+	});
+}
 
 // Connect to the db
 connection.connect((err)=>{
